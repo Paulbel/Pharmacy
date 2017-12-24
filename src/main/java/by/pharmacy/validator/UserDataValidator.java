15f80ab66c25c388b501dd705c826550795ad333
Paulbel <pavelsinelnikovbel@gmail.com>
2017-12-24 17:03:01 +0300

    manufacturer dao added

--- src/main/java/by/pharmacy/validator/UserDataValidator.java
+++ src/main/java/by/pharmacy/validator/UserDataValidator.java
@@ -5,8 +5,5 @@ import by.pharmacy.entity.User;
 import by.pharmacy.service.exception.ServiceException;
 
 public interface UserDataValidator {
-    public void checkPassword(User user, String password) throws ServiceException;
 
-
-    public void checkAccessRight(int id, Role role) throws ServiceException;
 }
--- src/main/java/by/pharmacy/validator/impl/UserDataValidatorImpl.java
+++ src/main/java/by/pharmacy/validator/impl/UserDataValidatorImpl.java
@@ -8,34 +8,10 @@ import by.pharmacy.entity.Role;
 import by.pharmacy.entity.User;
 import by.pharmacy.service.exception.AccessDeniedException;
 import by.pharmacy.service.exception.ServiceException;
-import by.pharmacy.service.exception.UserExistsException;
 import by.pharmacy.service.exception.WrongPasswordException;
 import by.pharmacy.validator.UserDataValidator;
 
 public class UserDataValidatorImpl implements UserDataValidator {
-    public UserDataValidatorImpl(){}
 
-    public void checkPassword(User user, String password) throws WrongPasswordException {
-        String userPassword = user.getPassword();
-        if(!userPassword.equals(password)){
-            throw new WrongPasswordException("Incorrect data!");
-        }
-    }
-
-
-
-    public void checkAccessRight(int id, Role role) throws ServiceException {
-        DAOFactory factory = DAOFactory.getInstance();
-        UserDAO userDAO = factory.getUserDAO();
-        try {
-            User user = userDAO.findUserById(id);
-            if (user.getRole() != role){
-                throw new AccessDeniedException("You have no rights for doing it!");
-            }
-        } catch (DAOException e) {
-            throw new ServiceException(e.getMessage(),e);
-        }
-
-    }
 
 }
