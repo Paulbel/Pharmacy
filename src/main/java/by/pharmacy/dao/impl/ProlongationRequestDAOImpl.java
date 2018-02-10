da750753cb44291d374447732e9091aa38d8dd5d
Paulbel <pavelsinelnikovbel@gmail.com>
2018-02-11 01:51:37 +0300

    prescription dao refactoring

--- src/main/java/by/pharmacy/dao/impl/ProlongationRequestDAOImpl.java
+++ src/main/java/by/pharmacy/dao/impl/ProlongationRequestDAOImpl.java
@@ -5,18 +5,20 @@ import by.pharmacy.dao.connectionpool.ConnectionPool;
 import by.pharmacy.dao.exception.DAOException;
 import by.pharmacy.entity.ProlongationRequest;
 import by.pharmacy.entity.ProlongationRequestStatus;
+import org.apache.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class ProlongationRequestDAOImpl implements ProlongationRequestDAO {
+    private final static ConnectionPool connectionPool = ConnectionPool.getInstance();
+    private final static Logger logger = Logger.getLogger(ConnectionPool.class);
     private final static String ADD_REQUEST = "INSERT INTO prolongation_request (prescription_id, day_count) VALUES (?,?)";
     private final static String CHANGE_STATUS = "UPDATE prolongation_request SET status = ? WHERE id = ?";
 
     @Override
     public void addProlongationRequest(ProlongationRequest request) throws DAOException {
-        ConnectionPool connectionPool = ConnectionPool.getInstance();
         Connection connection = connectionPool.getConnection();
         try (PreparedStatement statement = connection.prepareStatement(ADD_REQUEST)) {
 
@@ -24,20 +26,25 @@ public class ProlongationRequestDAOImpl implements ProlongationRequestDAO {
             statement.setInt(2, request.getTerm());
             statement.executeUpdate();
         } catch (SQLException e) {
-            throw new DAOException("happened", e);
+            logger.error("Not able to add prolongation request", e);
+            throw new DAOException("An error has occurred in attempt of adding prolongation request to database", e);
+        } finally {
+            connectionPool.closeConnection(connection);
         }
     }
 
     @Override
     public void changeProlongationRequestStatus(long requestId, ProlongationRequestStatus status) throws DAOException {
-        ConnectionPool connectionPool = ConnectionPool.getInstance();
         Connection connection = connectionPool.getConnection();
         try (PreparedStatement statement = connection.prepareStatement(CHANGE_STATUS)) {
             statement.setString(1, status.toString().toLowerCase());
             statement.setLong(2, requestId);
             statement.executeUpdate();
         } catch (SQLException e) {
-            throw new DAOException("happened", e);
+            logger.error("Not able to change prolongation request status", e);
+            throw new DAOException("An error has occurred in attempt of changing prolongation request status", e);
+        } finally {
+            connectionPool.closeConnection(connection);
         }
     }
 }
